f00094a0060d15019f5a4a50a54d291deb1f443a
Jesse <JesseAndCooper@Gmail.com>
2017-03-13 02:38:45 -0400

    Some various misc fixes to reflect changes on server side

--- js/socketHandler.js
+++ js/socketHandler.js
@@ -1,19 +1,20 @@
-const io = require('socket.io-client');
+// const io = require('socket.io-client');
 
 let startHandler;
 let actionHandler;
 let listUsersHandler;
 
 const startRoom = (e, socket, user, room) => {
+  document.querySelector('#btnAction').disabled = false;
   socket.emit('start', { roomname: room });
 };
 
 const sendAction = (e, socket, user, room, action) => {
   let target = document.querySelector('#target').value;
-    if (action === 'confess' || action === 'absolve') {  // Make sure that for certain actions user can only target themselves
+    if (action.value === 'confess' || action.value === 'absolve') {  // Make sure that for certain actions user can only target themselves
       target = user;
     }
-    socket.emit('action', { name: user, roomname: room, target: target, action: action });
+    socket.emit('action', { name: user, roomname: room, target: target, action: action.value });
 };
 
 const listUsers = (e, socket, user, room) => {
@@ -29,26 +30,29 @@ const connectSocket = (e) => {
   const log = document.querySelector('#log');
   const action = document.querySelector('#action');
 
+  log.innerHTML = '';
+
   socket.on('connect', () => {
     socket.emit('join', { name: user, roomname: room });
   });
 
   socket.on('win', () => {
     // Display win message
-    log.innerHTML += "You have won!";
+    log.innerHTML += "You have won!\n";
   });
 
   socket.on('loss', () => {
     // Display loss message
-    log.innerHTML += "You have been branded guilty!";
+    log.innerHTML += "You have been branded guilty!\n";
   });
 
   socket.on('notification', (data) => {
     // Display notifications from server
-    log.innerHTML += data.msg;
+    log.innerHTML += data.msg + '\n';
   });
 
   socket.on('confirm', (data) => {
+    console.log('Server says action confirmed: ' + data.confirm);
     // Disable turn controls
     if (data.confirm) {
       action.disabled = true;
@@ -57,8 +61,16 @@ const connectSocket = (e) => {
 
   socket.on('turnCount', (data) => {
     // Allow new action
-    log.innerHTML += `It is now turn ${data.turnNum}`;
+    log.innerHTML += `It is now turn ${data.turnNum}\n`;
     action.disabled = false;
+  });
+
+  socket.on('acquittalCount', (data) => {
+    // Display current acquittal count
+  });
+
+  socket.on('persuasionCount', (data) => {
+
   });
 
   socket.on('playerList', () => {
@@ -72,7 +84,7 @@ const connectSocket = (e) => {
   startHandler = () => startRoom(e, socket, user, room);
   document.querySelector('#btnStart').addEventListener('click', startHandler);
 
-  actionHandler = () => sendAction(e, socket, user, room, action.value);
+  actionHandler = () => sendAction(e, socket, user, room, action);
   document.querySelector('#btnAction').addEventListener('click', actionHandler);
 
   listUsersHandler = () => listUsers(e, socket, user, room);
